ddb1d083fd321b1a04dbf782c90c66f380a5fdf6
Jared VandeWater <jaredvande@gmail.com>
2021-07-23 19:57:52 -0600

    s

--- final/Repositories/KeepsRepository.cs
+++ final/Repositories/KeepsRepository.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using Dapper;
 using final.Models;
 
 namespace final.Repositories
@@ -13,24 +16,59 @@ namespace final.Repositories
       _db = db;
     }
 
+    // edit a keep
     internal int Put(Keep kData)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      UPDATE keeps SET
+      name = @Name
+      description = @Description
+      img = @Img
+      WHERE id = @id;";
+      return _db.Execute(sql, kData);
     }
 
+
+    // create a keep
     internal int Post(Keep newKeep)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO keeps
+      (creatorId, name, description, img)
+      VALUES( @CreatorId, @Name, @Description, @Img);
+      SELECT LAST_INSERT_ID();";
+      return _db.ExecuteScalar<int>(sql, newKeep);
     }
 
+
+    // get one keep
     internal Keep GetOne(int id)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      SELECT * FROM keeps
+      WHERE id = @id;";
+      return _db.QueryFirstOrDefault<Keep>(sql, new { id });
     }
 
-    internal Keep GetAll()
+
+
+    // gets all keeps and adds profile to them
+    public List<Keep> GetAll()
     {
-      throw new NotImplementedException();
+      string sql = @"
+                 SELECT 
+                    k.*,
+                    a.*
+                FROM keeps g
+                JOIN accounts a ON g.creatorId = a.id;
+            ";
+      return _db.Query<Keep, Profile, Keep>(sql, (k, p) =>
+           {
+             k.Creator = p;
+             return k;
+           }, splitOn: "id").ToList();
     }
   }
 }
+
+
