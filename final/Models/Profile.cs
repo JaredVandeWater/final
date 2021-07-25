1176b1aa17e0c847e8e3605045ccc2f1669ad4b7
Jared VandeWater <jaredvande@gmail.com>
2021-07-24 22:53:22 -0600

    k

--- final/Models/Profile.cs
+++ final/Models/Profile.cs
@@ -2,7 +2,7 @@ namespace final.Models
 {
   public class Profile
   {
-    public int Id { get; set; }
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Picture { get; set; }
